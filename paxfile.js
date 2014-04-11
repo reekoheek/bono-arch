9095957b3b1c9666a917379bb61313d3fff67091
Ganesha <reekoheek@gmail.com>
2014-04-11 14:02:07 +0700

    arg len validation

--- paxfile.js
+++ paxfile.js
@@ -5,6 +5,10 @@ module.exports = function() {
         Q = context.require('q');
 
     this.task('init', function(logger) {
+        if (arguments.length > 1) {
+            throw new Error('Wrong argument');
+        }
+
         logger.log('Updating composer...'.yellow);
         return cmd('composer', ['update']).then(function(stream) {
             console.log(stream[0]);
