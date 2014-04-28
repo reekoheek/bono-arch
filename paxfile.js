bbf828927fbc77adadae6649a3241b5ea725efd9
Ganesha <reekoheek@gmail.com>
2014-04-28 13:38:42 +0700

    include composer.lock

--- paxfile.js
+++ paxfile.js
@@ -1,17 +1,15 @@
 module.exports = function() {
-    var context = this;
-
-    var cmd = context.require('./utils/cmd'),
-        Q = context.require('q');
+    var cmd = this.require('./utils/cmd'),
+        Q = this.require('q');
 
     this.task('init', function(logger) {
         if (arguments.length > 1) {
             throw new Error('Wrong argument');
         }
 
-        logger.log('Updating composer...'.yellow);
-        return cmd('composer', ['update']).then(function(stream) {
-            console.log(stream[0]);
+        logger.head('Installing package from composer...');
+        return cmd('composer', ['install']).then(function(stream) {
+            logger.log(stream[0].trim());
         });
     });
 
